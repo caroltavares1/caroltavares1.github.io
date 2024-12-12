490690e51b627139c9a9896242873a71c46e79f6
Carolina Tavares <caroltavares.ts@gmail.com>
2024-12-12 10:44:09 -0300

    Fix bug no theme icon.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,21 +5,29 @@ const btn = document.querySelector('.theme-button');
 const savedTheme = localStorage.getItem('theme') || 'light-theme';
 document.body.classList.add(savedTheme);
 
+const savedThemeIcon = localStorage.getItem('theme-icon') || 'bi-moon-stars';
+document.getElementById('theme-icon').classList.add(savedThemeIcon)
+
 
 // Listen for a click on the button
 btn.addEventListener('click', function () {
   const currentTheme = document.body.classList[0]
   const newTheme = currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
 
+  const currentThemeIcon = document.getElementById('theme-icon').classList[1]
+  console.log(document.getElementById('theme-icon').classList[1])
+  const newThemeIcon = currentThemeIcon === 'bi-moon-stars' ? 'bi-sun' : 'bi-moon-stars';
+
   document.body.classList.remove(currentTheme);
   document.body.classList.add(newTheme);
 
+  const icon = document.querySelector('#theme-icon');
+  icon.classList.add(newThemeIcon);
+  icon.classList.remove(currentThemeIcon);
+
   localStorage.setItem('theme', newTheme); // Salva a preferência
+  localStorage.setItem('theme-icon', newThemeIcon); // Salva a preferência
 
-  const icon = document.querySelector('#theme-icon')
-  icon.classList.toggle('bi-sun')
-  icon.classList.toggle('bi-moon-stars')
-
-  console.log(document.body.classList[0])
+  console.log(document.getElementById('theme-icon').classList)
 
 })
